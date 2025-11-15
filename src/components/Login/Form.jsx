7c7148a385b544a8074b05ffc5516d1dbb3288a7
AL3-Richard-20 <monterorichard09@gmail.com>
2025-11-15 16:23:00 +0800

    Feat: wishlist page

--- src/components/Login/Form.jsx
+++ src/components/Login/Form.jsx
@@ -18,7 +18,7 @@ export default function Form(){
                         <input 
                             type="text" 
                             id="email-address-icon" 
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5" 
                             placeholder="Input username here" 
                             autocomplete="off" />
                     </div>
@@ -33,7 +33,7 @@ export default function Form(){
                         <input 
                             type="password" 
                             id="email-address-icon" 
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5" 
                             placeholder="Input username here" />
                     </div>
                 </div>
