f8fb321b1b5ffdc1036cd2aebf53009bd26c30a9
AL3-Richard-20 <monterorichard09@gmail.com>
2026-02-01 12:05:53 +0800

    Feat: Wishlist form submission

--- src/styles/tailwind-theme.jsx
+++ src/styles/tailwind-theme.jsx
@@ -4,6 +4,7 @@ const customTheme = createTheme({
     button: {
         color: {
             primary: "bg-blue-500 hover:bg-blue-600 text-white",
+            light: "bg-gray-100 hover:bg-gray-200 text-white",
         },
         size: {
             lg: "px-6 py-3 text-lg",
